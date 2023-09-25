4587d514a8634675a97b8980aa9f4ad67fb81940
richardilemon <richardiupeju16@gmail.com>
2023-09-25 12:43:16 -0700

    Big O

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,56 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - a function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ * @list: the list to sort
+ *
+ * Return: None
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *current;
+	int elem;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	for (temp = (*list)->next; temp != NULL; temp = temp->next)
+	{
+		elem = temp->n;
+		for (current = temp->prev; current != NULL; current = current->prev)
+		{
+			if (elem < current->n)
+			{
+				swap_nodes(current, temp);
+
+				if (current->prev == NULL)
+					*list = current;
+				else if (temp->prev == NULL)
+					*list = temp;
+				print_list(*list);
+
+				continue;
+			}
+		}
+	}
+}
+/**
+ * swap_nodes - function that swaps the nodes of a doubly linked list
+ * @nodeL: the left node
+ * @nodeR: the right node
+ *
+ * Return: None
+ */
+void swap_nodes(listint_t *nodeL, listint_t *nodeR)
+{
+	nodeL->next = nodeR->next;
+	nodeR->next = nodeL;
+	nodeR->prev = nodeL->prev;
+	nodeL->prev = nodeR;
+
+	if (nodeR->prev != NULL)
+		nodeR->prev->next = nodeR;
+	if (nodeL->next != NULL)
+		nodeL->next->prev = nodeL;
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,86 @@
+#include "sort.h"
+
+/**
+ * quick_sort - selection sort
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: None
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	qwiksort(array, 0, (int) size - 1, size);
+}
+
+/**
+ * qwiksort - quicksorts the array
+ * @array: the array
+ * @lo: low idx of the array
+ * @hi: hi idx of the array
+ * @size: size of the original array
+ *
+ * Return: None
+ */
+void qwiksort(int *array, int lo, int hi, size_t size)
+{
+	if (lo < hi)
+	{
+		/*pivot*/
+		int p = partition(array, lo, hi, size);
+
+		/*left partition*/
+		qwiksort(array, lo, p - 1, size);
+
+		/*right parititon*/
+		qwiksort(array, p + 1, hi, size);
+	}
+}
+
+/**
+ * partition - partitions the array
+ * @array: the array to partition
+ * @lo: low idx of the array
+ * @hi: hi idx of the array
+ * @size: size of the original array
+ *
+ * Return: returns the new pivot's idx
+ */
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int pivot = array[hi];
+	int i = (lo - 1), j;
+
+	for (j = lo; j <= hi - 1; j += 1)
+	{
+		if (array[j] < (int) pivot)
+		{
+			/* increment i then swap*/
+			i += 1;
+			swap(&array[i], &array[j]);
+
+			if (array[i] != array[j])
+				print_array(array, size);
+		}
+	}
+	swap(&array[i + 1], &array[hi]);
+	if (array[i + 1] != array[hi])
+		print_array(array, size);
+	return (i + 1);
+}
+
+/**
+ * swap - swaps the 2 elements of an array
+ * @a: item a
+ * @b: item b
+ *
+ * Return: None
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
